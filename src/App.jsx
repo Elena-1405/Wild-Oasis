782d96372e20aa9dba7655acb54687b34eed46eb
Elena Petrova <helenofspb@gmail.com>
2026-03-25 17:08:53 +0300

    Essential data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import styled from 'styled-components';
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  cursor: pointer;
+  background-color: purple;
+  color: white;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orange;
+  margin: 0;
+  padding: 0;
+`;
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => alert('Hello!')}>Hello</Button>
+      <Input placeholder='Number of guests' />
+    </StyledApp>
+  );
 }
 
 export default App;
