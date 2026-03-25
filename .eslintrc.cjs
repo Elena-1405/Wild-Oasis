e0b061d217fef61d2a523002102655f2a9ac5ffb
Elena Petrova <helenofspb@gmail.com>
2026-03-25 15:25:32 +0300

    environment setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,46 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+
   extends: [
     'eslint:recommended',
+
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+
+    'plugin:prettier/recommended',
   ],
+
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
   plugins: ['react-refresh'],
+
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+    'jsx-quotes': ['warn', 'prefer-single'],
+    'prettier/prettier': 'warn',
   },
-}
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+};
