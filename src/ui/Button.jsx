971427fd58cb6b497db268ea3cd12a6a4340db05
Elena Petrova <helenofspb@gmail.com>
2026-03-30 17:15:05 +0300

    Styled components created

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -49,19 +49,17 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
   border: none;
   border-radius: 7px;
-  cursor: pointer;
-  background-color: var(--color-brand-500);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
 
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
+  ${(props) => sizes[props.size]} //the same as in App
+  ${(props) => variations[props.variation]}
 `;
+
+Button.defaultProps = {
+  variation: 'primary',
+  size: 'medium',
+};
 
 export default Button;
