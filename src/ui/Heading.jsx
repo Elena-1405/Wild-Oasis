5300975ea594481264c122affd705ec8d7a8ac5d
Elena Petrova <helenofspb@gmail.com>
2026-03-30 17:14:06 +0300

    Fixes

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+
+const test = css`
+  text-align: center;
+`;
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === 'h1' &&
+    css`
+      font-size: 40px;
+      font-weight: 600;
+      background-color: yellow;
+    `}
+
+  ${(props) =>
+    props.as === 'h2' &&
+    css`
+      font-size: 20px;
+      font-weight: 600;
+      background-color: #efefa3;
+    `}
+
+    ${(props) =>
+    props.as === 'h3' &&
+    css`
+      font-size: 15px;
+      font-weight: 500;
+      background-color: #f7f7dc;
+    `}
+
+    line-height: 1.4;
+`;
+
+export default Heading;
