eb75c0ca72078a576926e92ce2da1bdba2233fb7
Elena Petrova <helenofspb@gmail.com>
2026-04-04 17:05:15 +0300

    Supabase installed && database created

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://eetadkdepjhzlglumsre.supabase.co';
+//const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = 'sb_publishable_N9EQoFr2kFwqLzm1W_85FQ_cMRi-yMc';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
