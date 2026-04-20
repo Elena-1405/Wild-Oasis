c1f923c82dfc784a6f5b9296f32b270ec2dcf2c8
Elena Petrova <helenofspb@gmail.com>
2026-04-20 16:22:51 +0300

    Uploading image to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eetadkdepjhzlglumsre.supabase.co';
+export const supabaseUrl = 'https://eetadkdepjhzlglumsre.supabase.co';
 //const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseKey = 'sb_publishable_N9EQoFr2kFwqLzm1W_85FQ_cMRi-yMc';
 const supabase = createClient(supabaseUrl, supabaseKey);
