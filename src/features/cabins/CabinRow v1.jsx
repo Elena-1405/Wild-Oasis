1e01f6296f5d77795629cd5f8b2d27ce605a549a
Elena Petrova <helenofspb@gmail.com>
2026-05-15 17:59:27 +0300

    Reusable context menu created

--- src/features/cabins/CabinRow v1.jsx
+++ src/features/cabins/CabinRow v1.jsx
@@ -74,28 +74,25 @@ function CabinRow({ cabin }) {
         <span>&mdash;</span>
       )}
       <div>
+        <button disabled={isCreating} onClick={handleDublicate}>
+          <HiSquare2Stack />
+        </button>
+
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
-            <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDublicate}>
-                Dub
-              </Menus.Button>
-
-              <Modal.Open opens='edit'>
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
-
-              <Modal.Open opens='delete'>
-                <Menus.Button icon={<HiTrash />}>Del</Menus.Button>
-              </Modal.Open>
-            </Menus.List>
-          </Menus.Menu>
-
+          <Modal.Open opens='edit'>
+            <button disabled={isDeleting}>
+              <HiPencil />
+            </button>
+          </Modal.Open>
           <Modal.Window name='edit'>
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
+          <Modal.Open opens='delete'>
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
           <Modal.Window name='delete'>
             <ConfirmDelete
               resourceName='cabins'
@@ -104,6 +101,17 @@ function CabinRow({ cabin }) {
             />
           </Modal.Window>
         </Modal>
+
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId} />
+          <Menus.List id={cabinId}>
+            <Menus.Button icon={<HiSquare2Stack />} onClick={handleDublicate}>
+              Dub
+            </Menus.Button>
+            <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+            <Menus.Button icon={<HiTrash />}>Del</Menus.Button>
+          </Menus.List>
+        </Menus.Menu>
       </div>
     </Table.Row>
   );
